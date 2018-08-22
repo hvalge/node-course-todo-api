6aeb9719a88dbcccf8fd04e627330875c1ae4e6c
unknown <hevalg@ttu.ee>
2018-08-22 20:39:30 +0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://hvalge:egh0cooN@host:port/dbname')
+// mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://hvalge:egh0cooN@host:port/dbname')
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
