a6fb46bef02843e5c55950d219b110e60960d3ae
unknown <hevalg@ttu.ee>
2018-08-22 17:26:58 +0300

    Heroku update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://user:pass@host:port/dbname' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose};
