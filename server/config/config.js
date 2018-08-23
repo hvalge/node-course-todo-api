a40477b859d3a8c3401dbfa2976942a52d8973f9
unknown <hevalg@ttu.ee>
2018-08-23 15:38:01 +0300

    Add POST /users route and token method

--- server/config/config.js
+++ server/config/config.js
@@ -7,4 +7,6 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+  process.env.MONGODB_URI = 'mongodb://hvalge:egh0cooN@ds125322.mlab.com:25322/nodejs-course-api'
 }
